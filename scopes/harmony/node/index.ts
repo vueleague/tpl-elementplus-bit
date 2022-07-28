bf539db914a4b26e1a7973a5795a9e23198b5e57
bengil <benjyg@gmail.com>
2022-07-28 15:23:01 +0300

    nodeAppType should be regular export (counter-intuitively, it's not a type!)

--- scopes/harmony/node/index.ts
+++ scopes/harmony/node/index.ts
@@ -1,4 +1,4 @@
 export type { NodeMain } from './node.main.runtime';
-export type { NodeAppType } from './node.app-type';
+export { NodeAppType } from './node.app-type';
 export type { NodeAppOptions, DeployContext } from './node-app-options';
 export { NodeAspect, NodeAspect as default } from './node.aspect';
